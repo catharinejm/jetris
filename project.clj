c440e4e28e74d1620495297575ecd0faa489f57f
Jon Distad <jon.distad@gmail.com>
2011-04-16 18:41:35 -0400

    FIRST POST!

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject jetris "1.0.0-SNAPSHOT"
+  :description "Tetris in CLojure"
+  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dev-dependencies [[swank-clojure "1.3.0"]])
---
+++ src/jetris/core.clj
@@ -0,0 +1,1 @@
+(ns jetris.core)
