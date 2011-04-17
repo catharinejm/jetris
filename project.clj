331c5159b8b95db654017e930f65f1056e67545d
Jon Distad <jon.distad@gmail.com>
2011-04-17 13:13:02 -0400

    Capture keyboard and move piece

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject jetris "1.0.0-SNAPSHOT"
   :description "Tetris in CLojure"
-  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.2.1"]
+		 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.3.0"]])
--- src/jetris/core.clj
+++ src/jetris/core.clj
@@ -1,1 +1,38 @@
-(ns jetris.core)
+(ns jetris.core
+  (:use [clojure.java.io :only (as-file)])
+  (:import [java.awt GraphicsEnvironment GraphicsDevice GraphicsConfiguration Graphics2D]
+	   [javax.swing JWindow JPanel JFrame KeyStroke AbstractAction]
+	   [java.awt.image BufferedImage]
+	   [java.awt.event KeyEvent]
+	   [javax.imageio ImageIO]))
+
+(def gfx-env (GraphicsEnvironment/getLocalGraphicsEnvironment))
+(def gfx-device (.getDefaultScreenDevice gfx-env))
+(def gfx-config (.getDefaultConfiguration gfx-device))
+
+(def sprite (atom
+	     {:x 0	     
+	      :y 0
+	      :image (ImageIO/read (as-file "resources/jetris_piece.gif"))}))
+
+(def pane
+  (proxy [JPanel] []
+    (paint [gfx]
+	   (.drawImage gfx
+		       (:image @sprite)
+		       (:x @sprite)
+		       (:y @sprite)
+		       nil))))
+(def frame (JFrame.))
+
+(.put (.getActionMap pane)
+      "space"
+      (proxy [AbstractAction] []
+	  (actionPerformed [e]
+			   (swap! sprite update-in [:x] inc)
+			   (.repaint frame))))
+(.put (.getInputMap pane)
+      (KeyStroke/getKeyStroke KeyEvent/VK_SPACE 0)
+      "space")
+
+(doto frame (.add pane) (.setSize 640 480) (.setVisible true))
